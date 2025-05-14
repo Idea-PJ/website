e33d971490a7c725af0f782cbb7f11babc7eb6e6
Ye Thiha <mightycryptowall@gmail.com>
2025-05-14 23:08:05 +0700

    add: SEOHeaders comp

---
+++ components/SEOHeaders.tsx
@@ -0,0 +1,57 @@
+import { defaults } from "@/utils/contants";
+import Head from "next/head";
+
+const { website_url, page_title, page_description, page_meta_image } = defaults;
+
+interface IProps {
+  title?: string;
+  description?: string;
+  img?: string;
+}
+
+const SEOHeaders: React.FC<IProps> = ({
+  title = page_title,
+  description = page_description,
+  img = page_meta_image,
+}) => {
+  const t = title ? `Idea PJ | ${title}` : "";
+  return (
+    <Head>
+      {/* <!-- Primary Meta Tags --> */}
+      <title>{t}</title>
+      <meta name="title" content={title} />
+      <meta
+        name="description"
+        content={description}
+      />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={website_url} />
+      <meta property="og:title" content={title} />
+      <meta
+        property="og:description"
+        content={description}
+      />
+      <meta
+        property="og:image"
+        content={img}
+      />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={website_url} />
+      <meta property="twitter:title" content={title} />
+      <meta
+        property="twitter:description"
+        content={description}
+      />
+      <meta
+        property="twitter:image"
+        content={img}
+      />
+    </Head>
+  );
+};
+
+export default SEOHeaders;
---
+++ utils/contants.ts
@@ -0,0 +1,6 @@
+export const defaults = {
+    website_url: "https://ideapj.com/",
+    page_title: "Idea PJ | Organisation",
+    page_description: "Build production ready app that follows the industry standards",
+    page_meta_image: "https://ideapj-statics.s3.us-east-1.amazonaws.com/images/ideapj-pagemeta.png"
+}
